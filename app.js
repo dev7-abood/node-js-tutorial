0ab7ae53f325ef0703595cc9635941e0cc65ef11
Abdulrhman H <aherzallah@cerebra.sa>
2022-05-04 19:44:51 +0300

    update the dependence

--- app.js
+++ app.js
@@ -7,20 +7,39 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const path = require("path");
 const routDir = require("./util/path");
-const port = process.env.PORT;
 
-app.use(bodyParser.urlencoded({extended : false}));
+
+// const sequelize = require('./config/database');
+const productsModel = require('./models/product');
+
+
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(routDir, 'public')));
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
-app.use('/' ,webRoutes);
-app.use('/admin' ,adminRoutes);
-app.use('/shop' ,shopRoutes);
+app.use('/', webRoutes);
+app.use('/admin', adminRoutes);
+app.use('/shop', shopRoutes);
 
 app.use((req, res, next) => {
-   res.status(404).sendFile(path.join(routDir, 'views', '404.html'));
+    res.status(404).sendFile(path.join(routDir, 'views', '404.html'));
 });
+
+const port = process.env.PORT;
+
+productsModel.sync().then(result => {
+    console.log(result)
+}).catch(err => {
+    console.log(err)
+})
+
+productsModel.create({
+    title : 'Abood',
+    price : 25.2,
+    imageUrl : 'imageUrl',
+    description : 'description'
+})
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
